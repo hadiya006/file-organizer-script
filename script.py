ae44fb84c4b165666510842407b078b896532add
hadiya006 <hadiyatahir3@gmail.com>
2026-07-02 17:51:22 +0500

    Add logging for file operations

--- script.py
+++ script.py
@@ -1,42 +1,50 @@
 from pathlib import Path
 import shutil
+import logging
 
+logging.basicConfig(
+    filename="organizer.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logging.info("Program started")
 folder=Path(r"C:\Users\hadiya\Downloads\TestFolder")
 for item in folder.iterdir():
     #print(item)
-    print("File Name:",item.name)
-    print("Extension:",item.suffix) #file extension
-    print("Size:",item.stat().st_size) #size of file in bytes
+    #print("File Name:",item.name)
+    #print("Extension:",item.suffix) #file extension
+    #print("Size:",item.stat().st_size) #size of file in bytes
     extension=item.suffix.lower()
     if extension==".jpg" or extension==".png":
-        print("Belongs to Images")
         destination=folder / "Images"
         if not destination.exists():
             destination.mkdir()
-            print("Image folder created.")
+            logging.info(f"Created folder: {destination.name}")
         else:
             print("Folder already exists")
         shutil.move(item,destination)
+        logging.info(f"Moving {item.name} to {destination.name}")
 
     elif extension==".pdf" or extension==".txt":
-        print("Belongs to Documents")
         destination=folder / "Docs"
         if not destination.exists():
             destination.mkdir()
-            print("Docs folder created.")
+            logging.info(f"Created folder: {destination.name}")
         else:
             print("Folder already exists")
         shutil.move(item,destination)
+        logging.info(f"Moving {item.name} to {destination.name}")
 
     elif extension==".mp3":
-        print("Belongs to Audio")
         destination=folder / "Audio"
         if not destination.exists():
             destination.mkdir()
-            print("Audio folder created.")
+            logging.info(f"Created folder: {destination.name}")
         else:
             print("Folder already exists")
         shutil.move(item,destination)
+        logging.info(f"Moving {item.name} to {destination.name}")
+
 
     print()
 
